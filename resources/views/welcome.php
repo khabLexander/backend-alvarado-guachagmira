cae3ec52641c69777994bf2e32f19de894ad0c96
Henrry <princedark393@gmail.com>
2021-07-20 14:39:31 -0500

    My change

---
+++ resources/views/welcome.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Henrry</title>
+</head>
+<body>
+    <h1>Backend con Laravel</h1>
+    <script languaje= "JavaScript" >
+        function programaJS1(){
+            const info = {
+                name: 'Api ',
+                integrantes: ' Henrry - Kevin',
+                city: ' Quito-Ec'
+            }
+            document.write(`${info.name}::${info.integrantes} >>>>${info.city}`);
+        }
+    </script>
+    <p>
+    <INPUT type=button name="button1"
+    value="Information"
+    onclick="programaJS1()">
+</body>
+</html>
