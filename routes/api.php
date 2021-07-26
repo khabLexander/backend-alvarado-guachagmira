568c5ef538f72c2aa0b2f8ffc76d333e63a357aa
Henrry <princedark393@gmail.com>
2021-07-25 23:52:37 -0500

    Routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::get('wineries', function () {
+    return ['Bodega1', 'bodega2'];
 });
+
+Route::get('wineries/{id}', function () {
+    return 'Bodega1';
+});
+
+Route::post('wineries', function () {
+    return 'bodega creada';
+});
+
+Route::put('wineries/{id}', function () {
+    return 'bodega actualizado';
+});
+Route::delete('wineries/{id}', function () {
+    return "bodega Eliminado";
+});
+
+
+
+Route::get('wineries/{id}/products', function () {
+    return ['productos1', 'productos2'];
+});
+
+Route::get('wineries/{id}/products/{code}', function () {
+    return 'producto1';
+});
+
+Route::post('wineries/{id}/products', function () {
+    return 'producto creado';
+});
+
+Route::put('wineries/{id}/products/{code}', function () {
+    return 'Producto actualizado';
+});
+Route::delete('wineries/{id}/products/{code}', function () {
+    return "Producto Eliminado";
+});
+
+
